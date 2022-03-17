6a8d760042c8acf8e064b92a61eec76d2f3507e5
Igor dos Santos Amado <igorsamado@gmail.com>
2022-03-17 00:41:20 -0300

    Corrigindo sender email nos testes

--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -58,13 +58,13 @@ class SubscribePostTest(TestCase):
 
     def test_subscription_email_from(self):
         email = mail.outbox[0]
-        expect = 'contato@eventex.com.br'
+        expect = 'contato@igorsantosamado.com.br'
 
         self.assertEqual(expect, email.from_email)
 
     def test_subscription_email_to(self):
         email = mail.outbox[0]
-        expect = ['contato@eventex.com.br', 'henrique@bastos.net']
+        expect = ['contato@igorsantosamado.com.br', 'henrique@bastos.net']
 
         self.assertEqual(expect, email.to)
 
